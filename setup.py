c282512fa1c49b4bd2a67aa8ec1f729829116802
Phil Underwood <beardydoc@gmail.com>
2017-07-23 16:56:31 +0100

    Install basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="TestEd",
+      version="0.1",
+      description="Python testing friendly editor",
+      author="Phil Underwood",
+      author_email="beardydoc@gmail.com",
+      url="http://github.com/furbrain/tested",
+      )  
