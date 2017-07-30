5a467e55090b6d27968247b0ba8d2a1559fa3500
Phil Underwood <beardydoc@gmail.com>
2017-07-30 22:01:32 +0100

    Start on syntaxtreevisitor

--- tested/languages/python.py
+++ tested/languages/python.py
@@ -1,11 +1,63 @@
 from ..plugins import PluginBase
+from collections import namedtuple
 import ast
+
+
+Entity = namedtuple("Entity", "line name")
+
+def getAliasName(node):
+    return node.asname or node.name
+
+class SyntaxTreeVisitor(ast.NodeVisitor):
+        
+    def visit_Import(self,node):
+        for f in node.names:
+            self.appendEntity(node.lineno,getAliasName(f))
+        self.generic_visit(node)
+        
+    def appendEntity(self,line,name):
+            self.entities.append(Entity(line=line, name=name))
+
+    def visit_ImportFrom(self,node):
+        self.visit_Import(node)
+        
+    def visit_Assign(self,node):
+        self.extractEntities(node.targets)
+                
+    def extractEntities(self,nodes):
+        for f in nodes:
+            if isinstance(f,ast.Name):
+                self.appendEntity(f.lineno,f.id)
+            if isinstance(f,ast.Tuple):
+                self.extractEntities(f.elts)
+ ###FIXME - need to include attribute and index accesses here...   
+ 
+    def visit_FunctionDef(self,node):
+        self.appendEntity(node.lineno,node.name)
+        for i in node.args.args:
+            self.appendEntity(node.lineno,i.id)
+        if node.args.vararg:
+            self.appendEntity(node.lineno,node.args.vararg)
+        if node.args.kwarg:
+            self.appendEntity(node.lineno,node.args.kwarg)
+        self.generic_visit(node)        
+        
+    def getEntities(self,node):
+        self.entities= []
+        self.visit(node)
+        return self.entities
 
 class PythonPlugin(PluginBase):
     def parseText(self, text):
         """Parse a python source file, generate a list of identifiers (classes, functions, variables)
            and possible completions for each of these"""
         self.parse_tree = ast.parse(text)
+        self.tree_visitor = SyntaxTreeVisitor()
+        print self.tree_visitor.getEntities(self.parse_tree)
+        self.entities = []
+        
+    def getCandidates(self, line_number, current_characters):
+        return []
         
       
 
--- tests/languages/test_python.py
+++ tests/languages/test_python.py
@@ -1,6 +1,8 @@
 import unittest
-from tested.languages.python import PythonPlugin
+from tested.languages.python import PythonPlugin, SyntaxTreeVisitor, getAliasName
 import os
+import ast
+import collections
 
 SPECIMEN_CODE = """
 #!/usr/bin/env python
@@ -15,7 +17,9 @@ from furtherpackage import pkg6 as pkg4
 antelope = 1000
 anteater = "insectivore"
 
-def simple_function(arg1,arg2):
+buzzard, beagle = "bird", "dog"
+
+def simple_function(arg1,arg2=None,*arg3,**arg4):
     local_var_f1 = (arg1+
         arg2)
     local_var_f2 = arg1-\
@@ -26,29 +30,94 @@ class BaseClass(AncestorClass):
     class_level_var = 100
     
     class SubClass:
-        def subclass_method(self,arg1,arg3):
-            return arg1+arg3
+        def subclass_method(self,arg5,arg6):
+            return arg5+arg6
             
-    def __init__(self, arg8):
+    def __init__(self, arg7):
         self.member_variable = arg8
                 
-    def instance_method(self,arg5,arg6):
-        self.contingent_member_variable = arg5
-        return arg5-arg6
+    def instance_method(self,arg8,arg9):
+        self.contingent_member_variable = arg8
+        return arg8-arg9
         
 late_variable = 100
 b = BaseClass(12)
 """ 
 
 class TestPython(unittest.TestCase):
-    def testInitWorks(self):
-        p = PythonPlugin()
-
-    def testParseWorks(self):
-        p = PythonPlugin()
-        p.parseText(SPECIMEN_CODE)
+    def setUp(self):
+        self.plugin = PythonPlugin()
+        self.plugin.parseText(SPECIMEN_CODE)
+        
+    def testSetupWorks(self):
+        pass
         
     def testNoCandidatesForSecondLine(self):
-        p = PythonPlugin()
-        p.parseText(SPECIMEN_CODE)
-        self.assertEqual([],p.getCandidates(1,""))
+        self.assertEqual([],self.plugin.getCandidates(1,""))
+    
+    @unittest.skip("Skip this for now")   
+    def testFirstImportIdentified(self):
+        self.assertEqual(["HighlyUseful"],self.plugin.getCandidates(2,""))
+        
+class TestSyntaxTreeVisitor(unittest.TestCase):
+    def setUp(self):
+        self.parse_tree = ast.parse(SPECIMEN_CODE)
+        self.visitor = SyntaxTreeVisitor()
+        
+    def testSimpleImportWorks(self):
+        names = [x.name for x in self.visitor.getEntities(self.parse_tree)]
+        self.assertIn("HighlyUseful.Lib",names)
+        
+    def testImportAs(self):
+        names = [x.name for x in self.visitor.getEntities(self.parse_tree)]
+        self.assertIn("pkg",names)
+
+    def testFrom(self):
+        names = [x.name for x in self.visitor.getEntities(self.parse_tree)]
+        self.assertIn("pkg3",names)
+
+    def testFromAs(self):
+        names = [x.name for x in self.visitor.getEntities(self.parse_tree)]
+        self.assertIn("pkg4",names)
+        
+    def testSimpleAssignment(self):
+        names = [x.name for x in self.visitor.getEntities(self.parse_tree)]
+        self.assertIn("antelope",names)
+        self.assertIn("anteater",names)
+        self.assertNotIn("antiquity",names)
+    
+    def testMultipleAssignment(self):
+        names = [x.name for x in self.visitor.getEntities(self.parse_tree)]
+        self.assertIn("buzzard",names)
+        self.assertIn("beagle",names)
+        
+    def testFunctionName(self):
+        names = [x.name for x in self.visitor.getEntities(self.parse_tree)]
+        self.assertIn("simple_function",names)
+        
+    def testFunctionArgs(self):
+        names = [x.name for x in self.visitor.getEntities(self.parse_tree)]
+        self.assertIn("arg1",names)
+        self.assertIn("arg2",names)
+        self.assertIn("arg3",names)
+        self.assertIn("arg4",names)
+        
+    def testLocalFunctionVars(self):
+        names = [x.name for x in self.visitor.getEntities(self.parse_tree)]
+        self.assertIn("local_var_f1",names)
+        self.assertIn("local_var_f2",names)
+    
+    
+        
+class TestGetAliasName(unittest.TestCase):
+    AliasMock = collections.namedtuple("AliasMock", "name asname")
+    def testSimpleCase(self):
+        sample = self.AliasMock("right",None)
+        self.assertEqual(getAliasName(sample),"right")
+        
+    def testAliasCase(self):
+        sample = self.AliasMock("wrong","right")
+        self.assertEqual(getAliasName(sample),"right")
+        
+    
+    
