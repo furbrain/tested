7bf704049623cc3c8c9474251077c94462e8b628
Phil Underwood <beardydoc@gmail.com>
2017-08-13 20:44:59 +0100

    Update __init__.py

--- tested/languages/python2/__init__.py
+++ tested/languages/python2/__init__.py
@@ -1,4 +1,5 @@
-from inferred_types import InferredType, InferredList, TypeSet
+from inferred_types import InferredType, InferredList, TypeSet, UnknownType
 from expressions import ExpressionTypeParser
 from statements import StatementBlockTypeParser
 from modules import ModuleTypeParser
+from functions import FunctionType
