4f29416373a832e9ce7fb9b4dba226dcd22b8d97
Phil Underwood <beardydoc@gmail.com>
2017-08-21 21:47:10 +0100

    Refactor scopeMatches -> Scope.matches

--- tested/languages/python3/scopes.py
+++ tested/languages/python3/scopes.py
@@ -3,25 +3,26 @@ import attr
 
 @attr.s
 class Scope():
-    line_start= attr.ib()
+    name = attr.ib()
+    line_start = attr.ib()
     line_end = attr.ib()
     indent = attr.ib()
     context = attr.ib()
 
-def scopeMatches(scope, line, indent):
-    if scope.line_start <= line <= scope.line_end:
-        if indent >= scope.indent:
-            return True
-    return False
+    def matches(self, line, indent):
+        if self.line_start <= line <= self.line_end:
+            if indent >= self.indent:
+                return True
+        return False
 
 class ScopeList():
     def __init__(self):
         self.scopes = []
-    def addScope(self, line_start, line_end, indent, context):
-        self.scopes.append(Scope(line_start, line_end, indent, context))
+    def addScope(self, name, line_start, line_end, indent, context):
+        self.scopes.append(Scope(name, line_start, line_end, indent, context))
         
     def getScope(self, line, indent):
-        possible_scopes = [x for x in self.scopes if scopeMatches(x, line, indent)]
+        possible_scopes = [x for x in self.scopes if x.matches(line, indent)]
         if possible_scopes:
             return sorted(possible_scopes, key=lambda x: x.indent)[-1]
         return None
--- tests/languages/python3/test_scopes.py
+++ tests/languages/python3/test_scopes.py
@@ -1,22 +1,22 @@
 import unittest
-from tested.languages.python3.scopes import Scope, ScopeList, scopeMatches
+from tested.languages.python3.scopes import Scope, ScopeList
 
 class TestScopeMatches(unittest.TestCase):
     def testBaseScopeMatches(self):
-        sc = Scope(0,100,0,{})
-        self.assertTrue(scopeMatches(sc,50,0))
+        sc = Scope('name',0,100,0,{})
+        self.assertTrue(sc.matches(50,0))
 
     def testIndentedScopeMatches(self):
-        sc = Scope(0,100,0,{})
-        self.assertTrue(scopeMatches(sc,50,4))
+        sc = Scope('name',0,100,0,{})
+        self.assertTrue(sc.matches(50,4))
         
     def testUnindentedScopeDoesNotMatch(self):    
-        sc = Scope(0,100,4,{})
-        self.assertFalse(scopeMatches(sc,50,0))
+        sc = Scope('name',0,100,4,{})
+        self.assertFalse(sc.matches(50,0))
 
     def testOutsideScopeDoesNotMacth(self):
-        sc = Scope(0,10,0,{})
-        self.assertFalse(scopeMatches(sc,50,0))
+        sc = Scope('name',0,10,0,{})
+        self.assertFalse(sc.matches(50,0))
 
 class TestScopeList(unittest.TestCase):
     def checkScope(self, scope_list, position, match):
@@ -28,31 +28,31 @@ class TestScopeList(unittest.TestCase):
 
     def testScopeListFindsSimpleMatch(self):
         dct = {'a':int}
-        self.checkScope([(0,10,0,dct)], (5,0), dct)
+        self.checkScope([('name',0,10,0,dct)], (5,0), dct)
 
     def testScopeListFindsSimpleIndentedMatch(self):
         dct = {'a':int}
-        self.checkScope([(0,10,0,dct)], (5,4), dct)
+        self.checkScope([('name',0,10,0,dct)], (5,4), dct)
         
     def testScopeListFindsIndentedMatch(self):
         dcta = {'a':int}
         dctb = {'b':float}
-        self.checkScope([(0,10,0,dcta), (2,8,4,dctb)], (5,4), dctb)
+        self.checkScope([('name',0,10,0,dcta), ('name',2,8,4,dctb)], (5,4), dctb)
 
     def testScopeListFindsUnIndentedMatch(self):
         dcta = {'a':int}
         dctb = {'b':float}
-        self.checkScope([(0,10,0,dcta), (2,8,4,dctb)], (9,0), dcta)
+        self.checkScope([('name',0,10,0,dcta), ('name',2,8,4,dctb)], (9,0), dcta)
         
     def testScopeListFailsWithWrongIndent(self):
         s = ScopeList()
         dcta = {'a':int}
-        s.addScope(0,10,4,dcta)
+        s.addScope('name',0,10,4,dcta)
         self.assertIsNone(s.getScope(5,0))
 
     def testScopeListFailsOutsideArea(self):
         s = ScopeList()
         dcta = {'a':int}
-        s.addScope(0,10,0,dcta)
+        s.addScope('name',0,10,0,dcta)
         self.assertIsNone(s.getScope(12,0))
         
