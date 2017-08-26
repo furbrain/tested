b5e58d9e4525e2b5b20175777f0f232aeec80bed
Phil Underwood <beardydoc@gmail.com>
2017-08-26 21:04:16 +0100

    Start work on automatic signature generation

---
+++ tested/languages/python3/utils/get_signatures.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import inspect
+import builtins
+
+
+def print_items(obj, indent=0):
+    if indent>12: return
+    blanks = " "*indent
+    for item, value in inspect.getmembers(obj):
+        if item=="__class__": continue
+        try:
+            print(blanks+"{0} ({1}): None".format(item, ', '.join(inspect.getargspec(value).args)))
+            continue
+        except TypeError:
+            pass
+        if inspect.isclass(value):
+            print(blanks+"Class: {0}".format(item))
+            print_items(value,indent+4)
+        else:
+            print(blanks+"Builtin: {0}".format(item))
+
+print_items(builtins)
