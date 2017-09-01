4519c41347dcecfdae76ef13fb363e9705ed2558
Phil Underwood <beardydoc@gmail.com>
2017-09-01 20:50:41 +0100

    Add parse functions

--- tested/languages/python3/__init__.py
+++ tested/languages/python3/__init__.py
@@ -7,3 +7,4 @@ from .classes import ClassType, InstanceType
 from .assignment import assign_to_node
 from .scopes import Scope, ScopeList
 from .builtins import get_global_scope, get_built_in_for_literal
+from .parse import parse_text, get_suggestions
---
+++ tested/languages/python3/parse.py
@@ -0,0 +1,43 @@
+import ast
+from . import modules, expressions
+
+def parse_text(text):
+    parser = modules.ModuleTypeParser()
+    return parser.parseModule(text)
+    
+def get_suggestions(context, line, line_number):
+    indent = len(line) - len(line.lstrip(' '))
+    scope = context.getScope(line_number, indent)
+    identifier = get_last_whole_identifier(line)
+    obj,_,prefix = identifier.rpartition('.')
+    if obj:
+        scope = expressions.get_expression_type(obj)    
+    return [x for x in scope if x.startswith(prefix)]
+    
+def get_last_whole_identifier(line):
+    for i in range(len(line)):
+        try:
+            result = ast.parse(line[i:], mode='eval')
+        except SyntaxError:
+            continue
+        else:
+            line = line[i:]
+            body = result.body
+            expr_type = type(body).__name__
+            if expr_type=="BinOp":
+                return get_line_part(line, body.right)
+            elif expr_type=="UnaryOp":
+                return get_line_part(line, body.operand)
+            elif expr_type=="Compare":
+                return get_line_part(line, body.comparators[-1]) 
+            elif expr_type=="Tuple":
+                return get_line_part(line, body.elts[-1])
+            return line
+    return ""
+    
+def get_line_part(line, node):
+    line = line[node.col_offset:]
+    return get_last_whole_identifier(line)
+    
+#do ifexp
+
