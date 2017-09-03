ec3cf3b0ce1b27d3c3e71021dc6976bab1f68571
Phil Underwood <beardydoc@gmail.com>
2017-09-03 14:48:55 +0100

    Add tests++

--- tests/languages/python3/test_types.py
+++ tests/languages/python3/test_types.py
@@ -1,8 +1,13 @@
 import unittest
 
-from tested.languages.python3 import InferredType, TypeSet, UnknownType
+from tested.languages.python3 import InferredType, TypeSet, UnknownType, FunctionType
 
 class TestInferredType(unittest.TestCase):
+    def setUp(self):
+        class TempClass:
+            pass
+        self.test_type = InferredType.fromType(TempClass)
+        
     def testInitWithSimpleVal(self):
         it_num = InferredType.fromType(1)
         it_str = InferredType.fromType("a")
@@ -35,56 +40,69 @@ class TestInferredType(unittest.TestCase):
         
     def testSingleAttributes(self):
         attr_dict = {'int':TypeSet(int),'str':TypeSet(str),'None':TypeSet(None)}
-        class TempClass:
-            pass
-        it = InferredType.fromType(TempClass)
         for name, tp in attr_dict.items():
-            it.add_attr(name, tp)
+            self.test_type.add_attr(name, tp)
         for name, tp in attr_dict.items():
-            self.assertEqual(it.get_attr(name), tp)
+            self.assertEqual(self.test_type.get_attr(name), tp)
          
     def testMultiAttributes(self):
         attr_dict = {'int':TypeSet(int), 'str':TypeSet(str), 'None':TypeSet(None)}
-        class TempClass:
-            pass
-        it = InferredType.fromType(TempClass)
         for tp in attr_dict.values():
-            it.add_attr('multi',tp)
-        self.assertEqual(it.get_attr('multi'), TypeSet(int,str,None))  
+            self.test_type.add_attr('multi',tp)
+        self.assertEqual(self.test_type.get_attr('multi'), TypeSet(int,str,None))  
         
     def testUnknownAttribute(self):
-        class TempClass:
-            pass
-        it = InferredType.fromType(TempClass)
-        self.assertEqual(it.get_attr('surprise'), TypeSet(UnknownType()))
+        self.assertEqual(self.test_type.get_attr('surprise'), TypeSet(UnknownType()))
         
     def testSingleItem(self):
-        class TempClass:
-            pass
-        it = InferredType.fromType(TempClass)
-        it.add_item(TypeSet(int))
-        self.assertEqual(it.get_item(12), TypeSet(int))
+        self.test_type.add_item(TypeSet(int))
+        self.assertEqual(self.test_type.get_item(12), TypeSet(int))
 
     def testMultiItem(self):
-        class TempClass:
-            pass
-        it = InferredType.fromType(TempClass)
-        it.add_item(TypeSet(int))
-        it.add_item(TypeSet(str))
-        self.assertEqual(it.get_item(12),TypeSet(int,str))
+        self.test_type.add_item(TypeSet(int))
+        self.test_type.add_item(TypeSet(str))
+        self.assertEqual(self.test_type.get_item(12),TypeSet(int,str))
         
     def testUnknownItem(self):
-        class TempClass:
-            pass
-        it = InferredType.fromType(TempClass)
-        self.assertEqual(it.get_item(12), UnknownType())
+        self.assertEqual(self.test_type.get_item(12), UnknownType())
+        
+    def testCallVia__call__(self):
+        func = FunctionType('test',[],TypeSet(int),'')
+        self.test_type.set_attr('__call__',func)
+        self.assertEqual(self.test_type.get_call_return([]),TypeSet(int))
         
     def testBadCall(self):
-       it = InferredType.fromType(int)
-       self.assertEqual(it.get_call_return(1), TypeSet(UnknownType()))
+        self.assertEqual(self.test_type.get_call_return([1]), TypeSet(UnknownType()))
 
+    def testAddType(self):    
+        it_num = InferredType.fromType(1)
+        it_num2  = InferredType.fromType(2)
+        it_str = InferredType.fromType("a")
+        self.assertEqual(it_num,it_num.add_type(it_num2))
+        self.assertNotEqual(it_num, it_num.add_type(it_str))
+        
+    def testGetAllAttrs(self):
+        it_num = InferredType.fromType(1)
+        it_str = InferredType.fromType("a")
+        self.test_type.add_attr('a',it_num)
+        self.test_type.add_attr('b',it_str)
+        self.test_type.add_attr('c',it_num)
+        self.test_type.add_attr('c',it_str)
+        self.assertEqual(self.test_type.get_all_attrs(),{'a':'<int>', 'b':'<str>', 'c':'<int>, <str>'})
+       
         
 class TestTypeSet(unittest.TestCase):
+    def setUp(self):
+        class TempClass1:
+            pass
+        class TempClass2:
+            pass
+        self.tc1 = InferredType.fromType(TempClass1)    
+        self.tc2 = InferredType.fromType(TempClass2)    
+        self.multiple_typeset = TypeSet(self.tc1, self.tc2)
+        self.int = InferredType.fromType(int)
+        self.str = InferredType.fromType(str)
+
     def testInitWithSingleVal(self):
         st = TypeSet(1)
         self.assertEqual(str(st),"<int>")
@@ -115,4 +133,40 @@ class TestTypeSet(unittest.TestCase):
         self.assertNotEqual(TypeSet(int, float), TypeSet(int))
         self.assertNotEqual(TypeSet(int, float), "int,float")
         self.assertNotEqual(TypeSet(int),[])
+
+    def testGetAttr(self): 
+        self.tc1.add_attr('tst',self.int)
+        self.tc2.add_attr('tst',self.str)
+        self.assertEqual(self.multiple_typeset.get_attr('tst'), TypeSet(self.int, self.str))
+
+    def testAddAttr(self): 
+        self.multiple_typeset.add_attr('tst', self.int)
+        self.assertEqual(self.tc1.get_attr('tst'), self.int)
+        self.assertEqual(self.tc2.get_attr('tst'), self.int)
+        
+    def testGetItem(self):
+        self.tc1.add_item(self.int)
+        self.tc2.add_item(self.str)
+        self.assertEqual(self.multiple_typeset.get_item(1), TypeSet(self.int, self.str))
+
+    def testAddItem(self): 
+        self.multiple_typeset.add_item(self.int)
+        self.assertEqual(self.tc1.get_item(1),self.int)
+        self.assertEqual(self.tc2.get_item(1),self.int)
+
+    def testGetCallReturn(self): 
+        func1 = FunctionType('f1',[],self.int,'')
+        func2 = FunctionType('f2',[],self.str,'')
+        ts = TypeSet(func1, func2)
+        self.assertEqual(ts.get_call_return([]), TypeSet(self.int, self.str))
+
+    def testGetAllAttrs(self): 
+        self.tc1.add_attr('a',self.int)
+        self.tc2.add_attr('b',self.str)
+        self.tc1.add_attr('c',self.int)
+        self.tc2.add_attr('c',self.str)
+        self.assertEqual(self.multiple_typeset.get_all_attrs(),{
+            'a':self.int, 
+            'b':self.str, 
+            'c':TypeSet(self.int, self.str)})
 
