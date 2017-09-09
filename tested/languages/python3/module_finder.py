c6f89f55f87846f7143b4deda5f678dff620ee0e
Phil Underwood <beardydoc@gmail.com>
2017-09-09 17:48:01 +0100

    Add code for system modules if local module not found and absolute reference

--- tested/languages/python3/module_finder.py
+++ tested/languages/python3/module_finder.py
@@ -1,13 +1,18 @@
 import os.path
+import importlib.util
 
 def find_module(path, level, from_file, initial_file):
     initial_dir = os.path.dirname(initial_file)
-    path = os.path.join(*path.split('.'))
-    possible_path = os.path.join(initial_dir,path)
+    file_path = os.path.join(*path.split('.'))
+    possible_path = os.path.join(initial_dir,file_path)
     if os.path.isdir(possible_path):
         possible_path = os.path.join(possible_path,'__init__')
     possible_path += '.py'
     if os.path.exists(possible_path):
         return possible_path
     else:
-        return None
+        if level==0:
+            spec = importlib.util.find_spec(path)
+            if spec:
+                return spec.origin
+    return None
--- tests/languages/python3/test_module_finder.py
+++ tests/languages/python3/test_module_finder.py
@@ -1,4 +1,5 @@
 import unittest
+import importlib.util
 import tested.languages.python3.module_finder as mf
 
 class TestModuleFinder(unittest.TestCase):
@@ -20,10 +21,16 @@ class TestModuleFinder(unittest.TestCase):
             '/home/test/Project/project/subb/__init__.py',
             '/home/test/Project/project/subb/mod2.py',
             ]
+        self.sys_modules = {
+            'sysmod': '/usr/lib/python/sysmod.py',
+            'test2.submod': '/usr/lib/python/test2/submod.py'
+        }    
         self.old_exists = mf.os.path.exists
         self.old_isdir = mf.os.path.isdir
+        self.old_find_spec = mf.importlib.util.find_spec
         mf.os.path.exists = self.dummy_exists
         mf.os.path.isdir = self.dummy_isdir
+        mf.importlib.util.find_spec = self.dummy_find_spec
         
     def dummy_exists(self, path):
         return path in self.files
@@ -33,10 +40,17 @@ class TestModuleFinder(unittest.TestCase):
             path = path+'/'
         return path in self.files
         
+    def dummy_find_spec(self, path):
+        if path in self.sys_modules:
+            spec = importlib.util.spec_from_file_location(path, self.sys_modules[path])
+            return spec
+        else:
+            return None
+        
     def tearDown(self):
         mf.os.path.exists = self.old_exists
         mf.os.path.isdir = self.old_isdir
-        
+        mf.importlib.util.find_spec = self.old_find_spec
     def check_finder(self, path, answer, level=0,
                      from_file = '/home/test/Project/project/main.py', 
                      initial_file = '/home/test/Project/project/main.py'):
@@ -55,6 +69,14 @@ class TestModuleFinder(unittest.TestCase):
     def testSubdirAbsoluteLooksup(self):
         self.check_finder('suba.mod1','/home/test/Project/project/suba/mod1.py')
     
+    def testSystemModuleLookup(self):
+        self.check_finder('sysmod', self.sys_modules['sysmod'])    
         
+    def testSystemSubModuleLookup(self):
+        self.check_finder('test2.submod', self.sys_modules['test2.submod'])
         
+    def testSystemModuleRelativeLookupFails(self):
+        self.check_finder('sysmod', None, level=1)    
+    
         
+
