8b9536ebaa8bf6bb712edcc72a43056d6ac07f6b
Phil Underwood <beardydoc@gmail.com>
2017-11-10 23:29:15 +0000

    Style fixes

--- tested/languages/python3/signatures.py
+++ tested/languages/python3/signatures.py
@@ -8,58 +8,58 @@ FUNC_PATTERN = r"""(?xm)
                    (?P<retval>.*)\n                 # return value
                    (?P<docstring> (?:[ ]{2}.*?\n)*) # following docstring"""
 
-ATTRIBUTE_PATTERN =r"(?m)^(?P<attrname>\w+) = (?P<value>.*)$"
+ATTRIBUTE_PATTERN = r"(?m)^(?P<attrname>\w+) = (?P<value>.*)$"
 
 def split_on(text, split):
     """return a string split on "split" characters provided they are not within brackets"""
     bracket_queue = []
-    brackets = {'}':'{',
-                ']':'[',
-                ')':'(',
-                '>':'<'}
+    brackets = {'}': '{',
+                ']': '[',
+                ')': '(',
+                '>': '<'}
     result = [""]
     for i, char in enumerate(text):
         if char in brackets.values():
             bracket_queue.append(char)
         if char in brackets.keys():
-            if bracket_queue and brackets[char]==bracket_queue[-1]:
+            if bracket_queue and brackets[char] == bracket_queue[-1]:
                 bracket_queue.pop()
             else:
                 raise AttributeError('Unmatched bracket')
-        if char==split and len(bracket_queue)==0:
+        if char == split and len(bracket_queue) == 0:
             result.append("")
         else:
             result[-1] += char
     return result
 
-def is_bracketed(text,brackets):
-    return text[0]==brackets[0] and text[-1]==brackets[-1]
+def is_bracketed(text, brackets):
+    return text[0] == brackets[0] and text[-1] == brackets[-1]
 
 def read_type(text, scope):
     text = text.strip()
-    #first split on pipes...
+    # first split on pipes...
     pipes = split_on(text, '|')
-    if len(pipes)>1:
+    if len(pipes) > 1:
         subtypes = [read_type(x, scope) for x in pipes]
         return inferred_types.TypeSet(*subtypes)
     text = pipes[0].strip()
 
-    #return tuples
-    if is_bracketed(text,'()'):
+    # return tuples
+    if is_bracketed(text, '()'):
         text = text[1:-1]
         subtypes = [read_type(x, scope) for x in split_on(text, ',')]
         return inferred_types.InferredTuple(*subtypes)
 
-    #return lists
-    if is_bracketed(text,'[]'):
+    # return lists
+    if is_bracketed(text, '[]'):
         text = text[1:-1]
         return inferred_types.InferredList(read_type(text, scope))
 
-    #return sets/dicts
-    if is_bracketed(text,'{}'):
+    # return sets/dicts
+    if is_bracketed(text, '{}'):
         text = text[1:-1]
-        parts = split_on(text,':')
-        if len(parts)>1:
+        parts = split_on(text, ':')
+        if len(parts) > 1:
             return inferred_types.InferredDict([read_type(parts[0], scope)], [read_type(parts[1], scope)])
         else:
             return inferred_types.InferredSet(read_type(text, scope))
@@ -67,7 +67,7 @@ def read_type(text, scope):
     type_name = '<{}>'.format(text.strip())
     if type_name in scope:
         return scope[type_name]
-    elif type_name=="<Unknown>":
+    elif type_name == "<Unknown>":
         return inferred_types.UnknownType()
     else:
         raise AttributeError('Unknown builtin type {}'.format(type_name))
@@ -87,12 +87,9 @@ def read_function(matching_regex, scope):
                                   docstring=docstring)
     return None
 
-
-
 def read_spec(text, scope):
     funcs = [read_function(x, scope) for x in re.finditer(FUNC_PATTERN, text)]
-    funcs = {x.name:x for x in funcs}
-    attributes = {x[0]:read_type(x[1], scope) for x in re.findall(ATTRIBUTE_PATTERN, text)}
+    funcs = {x.name: x for x in funcs}
+    attributes = {x[0]: read_type(x[1], scope) for x in re.findall(ATTRIBUTE_PATTERN, text)}
     attributes.update(funcs)
     return attributes
-
