950f1af0096c8eadea9b6b1e9a8e0d0cfe4417de
Phil Underwood <beardydoc@gmail.com>
2017-11-10 21:43:08 +0000

    Style fixes

--- tested/languages/python3/document.py
+++ tested/languages/python3/document.py
@@ -1,9 +1,10 @@
-from .modules import ModuleType
+from . import modules
+
 class Document:
     def __init__(self, text, location):
         self.text = text
         self.location = location
-        self.module = ModuleType.fromText(text, location, self)
+        self.module = modules.ModuleType.fromText(text, location, self)
         self.module.parseText(text)
         self.scopes = self.module.scope_list
 
--- tested/languages/python3/expressions.py
+++ tested/languages/python3/expressions.py
@@ -1,81 +1,79 @@
 import ast
 import warnings
 
-from .inferred_types import TypeSet, InferredList, InferredTuple, InferredSet, InferredDict, InferredType, InferredIterator, UnknownType, get_type_name
-from .builtins import get_built_in_for_literal, create_list, create_dict, create_set, create_tuple
-from .scopes import Scope
+from . import inferred_types, builtins, scopes, utils
 
 def get_expression_type(expression, scope):
     if scope is None:
         warnings.warn("No scope passed to get_expression_type with expression: {}".format(expression))
-    if isinstance(expression,str):
+    if isinstance(expression, str):
         expression = ast.parse(expression)
     parser = ExpressionTypeParser(scope)
-    return parser.getType(expression)
+    return parser.get_type(expression)
 
 class ExpressionTypeParser(ast.NodeVisitor):
     def __init__(self, scope):
         self.scope = scope
-        self.float = get_built_in_for_literal(1.1)
-        self.int = get_built_in_for_literal(1)
-        self.str = get_built_in_for_literal('a')
-        self.bool = get_built_in_for_literal(True)
+        self.float = builtins.get_built_in_for_literal(1.1)
+        self.int = builtins.get_built_in_for_literal(1)
+        self.str = builtins.get_built_in_for_literal('a')
+        self.bool = builtins.get_built_in_for_literal(True)
         self.NUMERIC_TYPES = (self.int, self.float)
         
-    def getType(self, expression):
+    def get_type(self, expression):
         result = self.visit(expression)
         if result is None:
             warnings.warn("Unimplemented code: {}".format(ast.dump(expression)))
-            return UnknownType()
+            return inferred_types.UnknownType()
         return result
                 
     def visit_Num(self, node):
-        return get_built_in_for_literal(node.n)
+        return builtins.get_built_in_for_literal(node.n)
         
     def visit_Str(self, node):
-        return get_built_in_for_literal(node.s)
+        return builtins.get_built_in_for_literal(node.s)
         
     def visit_Bytes(self, node):
-        return get_built_in_for_literal(node.s)
+        return builtins.get_built_in_for_literal(node.s)
         
     def visit_Name(self, node):
         if node.id in self.scope:
             return self.scope[node.id]
         else:
-            return UnknownType()
+            return inferred_types.UnknownType()
     
     def visit_NameConstant(self, node):
-        return get_built_in_for_literal(node.value)
+        return builtins.get_built_in_for_literal(node.value)
            
     def visit_List(self, node):
         items = self.get_sequence_items(node.elts)
-        return create_list(*items)
+        return builtins.create_list(*items)
         
     def visit_Tuple(self, node):
         items = self.get_sequence_items(node.elts)
-        return create_tuple(*items)
+        return builtins.create_tuple(*items)
         
     def visit_Set(self, node):
         items = self.get_sequence_items(node.elts)
-        return create_set(*items)
+        return builtins.create_set(*items)
 
     def get_sequence_items(self, node_list):
         items = []
         for node in node_list:
-            if isStarred(node):
-                node_type = self.getType(node.value)
+            if utils.is_ast_starred(node):
+                node_type = self.get_type(node.value)
                 items.extend(node_type.get_star_expansion())
             else:
-                items.append(self.getType(node))
+                items.append(self.get_type(node))
         return items
         
     def visit_Dict(self, node):
-        keys = [self.getType(key) for key in node.keys]
-        items = [self.getType(value) for value in node.values]
-        return create_dict(keys, items)
+        keys = [self.get_type(key) for key in node.keys]
+        items = [self.get_type(value) for value in node.values]
+        return builtins.create_dict(keys, items)
 
     def visit_Call(self, node):
-        func_types = self.getType(node.func)
+        func_types = self.get_type(node.func)
         args = self.get_sequence_items(node.args)
         return func_types.get_call_return(args)
         
@@ -84,60 +82,60 @@ class ExpressionTypeParser(ast.NodeVisitor):
         return FunctionType.fromLambdaNode(node, self.scope)    
         
     def visit_Attribute(self, node):
-        base_var = self.getType(node.value)
+        base_var = self.get_type(node.value)
         return base_var.get_attr(node.attr)
                 
     def visit_Expr(self, node):
-        return self.getType(node.value)
+        return self.get_type(node.value)
         
     def visit_Module(self, node):
-        return self.getType(node.body[0])
+        return self.get_type(node.body[0])
         
     def visit_IfExp(self, node):
-        return TypeSet(self.getType(node.body), self.getType(node.orelse))
+        return inferred_types.TypeSet(self.get_type(node.body), self.get_type(node.orelse))
             
     def visit_BinOp(self, node):
         op = type(node.op).__name__
-        result = TypeSet()
-        for left in self.getType(node.left):
-            for right in self.getType(node.right):
-                new_type = self.getBinOpType(left, right, op)
+        result = inferred_types.TypeSet()
+        for left in self.get_type(node.left):
+            for right in self.get_type(node.right):
+                new_type = self.get_binary_op_type(left, right, op)
                 if new_type is not TypeError:
                     result = result.add_type(new_type)
         return result
             
-    def getBinOpType(self, left, right, op):
-        if self.bothArgsNumeric(left,right):
-            return self.getHighestPriorityNumber(left, right)
-        if self.bothArgsStrings(left, right):
+    def get_binary_op_type(self, left, right, op):
+        if self.both_args_numeric(left, right):
+            return self.get_highest_priority_number(left, right)
+        if self.both_args_strings(left, right):
             return self.str
-        if left == self.str and right in self.NUMERIC_TYPES and op=="Mult":
+        if left == self.str and right in self.NUMERIC_TYPES and op == "Mult":
             return left
-        if left == self.str and op=="Mod":
+        if left == self.str and op == "Mod":
             return left
         return TypeError
         
-    def bothArgsNumeric(self,left,right):
+    def both_args_numeric(self, left, right):
         return left in self.NUMERIC_TYPES and right in self.NUMERIC_TYPES
             
-    def bothArgsStrings(self, left, right):
+    def both_args_strings(self, left, right):
         return left == self.str and right == self.str
         
-    def getHighestPriorityNumber(self, left, right):
-        if self.float in (left,right):
+    def get_highest_priority_number(self, left, right):
+        if self.float in (left, right):
             return self.float
         else:
             return self.int
     
     def visit_UnaryOp(self, node):
         op = type(node.op).__name__
-        if op=="Not":
+        if op == "Not":
             return self.bool
-        if op=="Invert":
+        if op == "Invert":
             return self.int
-        if op in ("UAdd","USub"):
-            result = TypeSet()
-            for operand in self.getType(node.operand):
+        if op in ("UAdd", "USub"):
+            result = inferred_types.TypeSet()
+            for operand in self.get_type(node.operand):
                 if operand in self.NUMERIC_TYPES:
                     result = result.add_type(operand)
                 else:
@@ -148,15 +146,15 @@ class ExpressionTypeParser(ast.NodeVisitor):
         return self.bool
         
     def visit_Subscript(self, node):
-        value = self.getType(node.value)
+        value = self.get_type(node.value)
         slice_type = type(node.slice).__name__
-        if slice_type=="Index":
+        if slice_type == "Index":
             index = node.slice.value
             index_type = type(node.slice.value).__name__
-            if index_type=="Num":
+            if index_type == "Num":
                 return value.get_item(index.n)
             else:
-                return value.get_item(self.getType(index))
+                return value.get_item(self.get_type(index))
         else:
             if hasattr(value, 'get_slice'):
                 return value.get_slice()
@@ -166,31 +164,31 @@ class ExpressionTypeParser(ast.NodeVisitor):
         return self.bool
         
     def visit_ListComp(self, node):
-        scope = self.getScopeForComprehension(node)
+        scope = self.get_scope_for_comprehension(node)
         target = get_expression_type(node.elt, scope)
-        return create_list(target)
+        return builtins.create_list(target)
             
     def visit_SetComp(self, node):
-        scope = self.getScopeForComprehension(node)
+        scope = self.get_scope_for_comprehension(node)
         target = get_expression_type(node.elt, scope)
-        return create_set(target)
+        return builtins.create_set(target)
             
     def visit_DictComp(self, node):
-        scope = self.getScopeForComprehension(node)
+        scope = self.get_scope_for_comprehension(node)
         key_target = get_expression_type(node.key, scope)
         value_target = get_expression_type(node.value, scope)
-        return create_dict([key_target],[value_target])
+        return builtins.create_dict([key_target], [value_target])
         
     def visit_GeneratorExp(self, node):
-        scope = self.getScopeForComprehension(node)
+        scope = self.get_scope_for_comprehension(node)
         target = get_expression_type(node.elt, scope)
-        return InferredIterator(target)
+        return inferred_types.InferredIterator(target)
     
-    def getScopeForComprehension(self, node):
+    def get_scope_for_comprehension(self, node):
         from .assignment import assign_to_node
         scope = self.scope
         for generator in node.generators:
-            scope = Scope('__listcomp__', node.lineno, node.col_offset, parent=scope)
+            scope = scopes.Scope('__listcomp__', node.lineno, node.col_offset, parent=scope)
             iterator = get_expression_type(generator.iter, scope)
             assign_to_node(generator.target, iterator.get_iter(), scope)
         return scope    
