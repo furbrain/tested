583ad8917f7c558b6d897d2ecef847e20dedd35d
Phil Underwood <beardydoc@gmail.com>
2017-11-19 20:18:30 +0000

    Misc changes

--- tested/itypes/__init__.py
+++ tested/itypes/__init__.py
@@ -3,4 +3,4 @@ from .compound import InferredIterator
 from .builtins import get_global_scope, get_type_by_name, get_type_by_value, create_list, create_set, create_dict, create_tuple
 from .functions import FunctionType
 from .classes import ClassType
-from .scopes import Scope, ScopeList
+
